a65d423df01a9eccde0bee5f0c85e757f47b9e73
akash <akashksinha7115@gmail.com>
2024-04-15 13:35:14 +0530

    fnal commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+fn is_positive_integer_present(arr: &Vec<i32>) -> Option<i32> {
+    let n = arr.len();
+    let mut min_positive = std::i32::MAX;
+    
+    for i in 0..n {
+        if arr[i] > 0 && arr[i] < min_positive {
+            min_positive = arr[i];
+        }
+    }
+    
+    if min_positive != std::i32::MAX {
+        return Some(min_positive);
+    } else {
+        return None;
+    }
+}
+
+
+
+
+
+fn main() {
+    let arr = vec![-1, -2, -3, 0, -5, -6];
+  
+    match is_positive_integer_present(&arr) {
+        Some(val) => println!("Smallest positive integer is: {}", val),
+        None => println!("No number is bigger than 0"),
+    }
+}
